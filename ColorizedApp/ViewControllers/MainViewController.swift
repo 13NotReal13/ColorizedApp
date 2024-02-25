03d23cf11702f02aca02ef7b9c11bac20f856bf3
Ivan Semikin <i.semikin.94@icloud.com>
2024-02-25 12:34:52 +0100

    Update MainVC with create protocol and delegate

--- ColorizedApp/ViewControllers/MainViewController.swift
+++ ColorizedApp/ViewControllers/MainViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+protocol SettingsViewControllerDelegate: AnyObject {
+    func setColor(_ color: UIColor)
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class MainViewController: UIViewController {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let settingsVC = segue.destination as? SettingsViewController
+        settingsVC?.delegate = self
+        settingsVC?.viewColor = view.backgroundColor
     }
-
 
 }
 
+extension MainViewController: SettingsViewControllerDelegate {
+    func setColor(_ color: UIColor) {
+        view.backgroundColor = color
+    }
+}
